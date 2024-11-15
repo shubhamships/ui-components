ab29b11613b6565826dbca87aba1cdee6fface35
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-15 11:24:11 +0530

    color changed

--- aayushi/src/components/Input.jsx
+++ aayushi/src/components/Input.jsx
@@ -10,7 +10,7 @@ export const Input = ({ type, placeholder, className = "", value, onChange, id, 
         onChange={onChange}
         id={id}
         name={name}
-        className={`border rounded-md w-full py-2 px-3 flex h-10 text-sm focus-visible:outline-none focus-visible:ring-1 focus:ring-[#1F499E] ${className}`}
+        className={`border rounded-md w-full py-2 px-3 flex h-10 text-sm focus-visible:outline-none focus-visible:ring-1 focus:ring-blue-800 ${className}`}
       />
     </div>
   );
