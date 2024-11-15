1a692dc90f650d72a4f6bdfce4ce0221aa81e61d
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-15 15:06:19 +0530

    prettierrc files added

--- aayushi/src/App.jsx
+++ aayushi/src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Aayushi! test</h1>;
+  return (
+<div>
+<h1 className="text-3xl font-bold underline">Hello Aayushi!</h1>
+<button >click</button>
+
+</div>
+  )
+  
+  
 }
 
 export default App;
