47d9b938478d176e7563c68a8a9f490ea353f49a
Aman <aman.tripathi@shipglobal.in>
2024-11-19 16:23:58 +0530

    extention changed from js to ts

---
+++ aman/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.ts";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+if (rootElement)
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
