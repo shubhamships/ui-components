689b83c7f448bb3757204ca0fb4d51ea190de621
ujjwal105 <ujjwal.tyagi@shipglobal.in>
2024-11-20 18:54:58 +0530

    input basic components

--- ujjwal/src/App.tsx
+++ ujjwal/src/App.tsx
@@ -1,11 +1,207 @@
-
 import { useState } from "react";
 import FormInput from "./componets/FormInput";
+
+// input basic components
+// floating input
+// submit type
+// hidden
+//Types
+
 function App() {
-  const [data,setData] = useState()
-  return <div>
-    <FormInput type="email" value={data} onChange={()=>alert("Hello")} label="email">Submit</FormInput>
-  </div>
+  const [data, setData] = useState();
+
+  return (
+    <div className="bg-gray-200 h-full">
+      {/* Submit  */}
+      <div className="grid grid-cols-1  lg:grid-cols-4 md:grid-cols-2 p-8 pr-8  gap-6 justify-evenly items-center">
+        <FormInput type="text" value={data} label="First Name" variant="default" size="sm">
+          Submit
+        </FormInput>
+        <FormInput type="text" value={data} label="Last name" variant="default" size="md">
+          Submit
+        </FormInput>{" "}
+        <FormInput type="text" value={data} label="Company" variant="default" size="lg">
+          Submit
+        </FormInput>{" "}
+        <FormInput type="text" value={data} label="Team" variant="default" size="xl">
+          Submit
+        </FormInput>{" "}
+      </div>
+
+      <div className="grid grid-cols-1 mt-10 lg:grid-cols-4 md:grid-cols-2 p-8 pr-8  gap-6 justify-evenly items-center">
+        <FormInput
+          type="email"
+          value={data}
+          label="email"
+          placeholder="example@gmail.com"
+          variant="red"
+          size="sm"
+          icon="mail"
+        >
+          button
+        </FormInput>
+        <FormInput
+          type="email"
+          value={data}
+          label="email"
+          placeholder="example@gmail.com"
+          variant="red"
+          size="md"
+          icon="mail"
+        >
+          button
+        </FormInput>{" "}
+        <FormInput
+          type="email"
+          value={data}
+          label="email"
+          placeholder="example@gmail.com"
+          variant="red"
+          size="lg"
+          icon="mail"
+        >
+          button
+        </FormInput>
+        <FormInput
+          type="email"
+          value={data}
+          label="email"
+          placeholder="example@gmail.com"
+          variant="red"
+          size="xl"
+          icon="mail"
+        >
+          button
+        </FormInput>
+      </div>
+      <div className="grid grid-cols-1 mt-10 lg:grid-cols-4 md:grid-cols-2 p-8 pr-8  gap-6 justify-evenly items-center">
+        <FormInput
+          type="number"
+          value={data}
+          label="number"
+          placeholder="123-456-7890"
+          variant="green"
+          size="sm"
+          icon="number"
+        >
+          button
+        </FormInput>{" "}
+        <FormInput
+          type="number"
+          value={data}
+          label="number"
+          placeholder="123-456-7890"
+          variant="green"
+          size="md"
+          icon="number"
+        >
+          button
+        </FormInput>{" "}
+        <FormInput
+          type="number"
+          value={data}
+          label="number"
+          placeholder="123-456-7890"
+          variant="green"
+          size="lg"
+          icon="number"
+        >
+          button
+        </FormInput>{" "}
+        <FormInput
+          type="number"
+          value={data}
+          label="number"
+          placeholder="123-456-7890"
+          variant="green"
+          size="xl"
+          icon="number"
+        >
+          button
+        </FormInput>
+      </div>
+      <div className="grid grid-cols-1 mt-10 lg:grid-cols-4 md:grid-cols-2 p-8 pr-8  gap-6 justify-evenly items-center">
+        <FormInput
+          type="search"
+          value={data}
+          label="search"
+          placeholder="Choose File"
+          variant="blue"
+          size="sm"
+          icon="mail"
+        >
+          button
+        </FormInput>{" "}
+        <FormInput
+          type="search"
+          value={data}
+          label="search"
+          placeholder="Choose File"
+          variant="blue"
+          size="md"
+          icon="mail"
+        >
+          button
+        </FormInput>{" "}
+        <FormInput
+          type="search"
+          value={data}
+          label="search"
+          placeholder="Choose File"
+          variant="blue"
+          size="lg"
+          icon="location"
+        >
+          button
+        </FormInput>{" "}
+        <FormInput
+          type="search"
+          value={data}
+          label="search"
+          placeholder="Search"
+          variant="blue"
+          size="xl"
+          icon="search"
+        >
+          button
+        </FormInput>
+      </div>
+
+      {/* <form className="grid grid-cols-1 mt-10 lg:grid-cols-4 md:grid-cols-2 p-8 pr-8  gap-6 justify-evenly items-center">
+        <FormInput type="hidden" value={"Hello"} label="hidden" placeholder="Enter value" variant="default">
+          Select
+        </FormInput>{" "}
+        <FormInput type="hidden" value={"Hello"} label="hidden" placeholder="Enter value" variant="default">
+          Select
+        </FormInput>{" "}
+        <FormInput type="hidden" value={"Hello"} label="hidden" placeholder="Enter value" variant="default">
+          Select
+        </FormInput>{" "}
+        <FormInput type="hidden" value={"Hello"} label="hidden" placeholder="Enter value" variant="default">
+          Select
+        </FormInput>
+      </form> */}
+
+      {/* 
+     
+      <FormInput type="submit" value={data} label="submit" placeholder="Choose File">
+        button
+      </FormInput>
+      <FormInput type="button" value={data} label="button" placeholder="Choose File">
+        button
+      </FormInput>
+      
+      <FormInput type="number" value={data} label="number" placeholder="Choose File">
+        button
+      </FormInput>
+      <FormInput type="password" value={data} label="password" placeholder="Choose File">
+        button
+      </FormInput>
+      <FormInput type="search" value={data} label="search" placeholder="Choose File">
+        button
+      </FormInput> */}
+    </div>
+  );
 }
 
 export default App;
--- ujjwal/src/componets/FormInput.tsx
+++ ujjwal/src/componets/FormInput.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import { Mail } from "lucide-react";
+import { MapPin } from "lucide-react";
+import { Hash } from "lucide-react";
+import { Search } from "lucide-react";
 
 interface FormInputProps {
   name?: string;
   type: string;
-  label: string;
+  label?: string;
   placeholder?: string;
   value: any;
-  onChange: () => void;
+  onChange?: () => void;
   isRequired?: boolean;
-  children:any
+  children: any;
+  className?: string;
+  variant?: keyof typeof colorCombination;
+  size?: keyof typeof InputSize;
+  icon?: keyof typeof Icon;
 }
+
+const colorCombination = {
+  default: "text-gray-800 bg-gray-100 p-2 hover:bg-gray-200 focus:ring-1 focus:ring-green-400",
+  blue: "text-black p-2 focus:ring-1 focus:ring-blue-400",
+  green: "text-black p-2 focus:ring-1 focus:ring-green-400",
+  yellow: "text-black p-2 focus:ring-1 focus:ring-yellow-400",
+  red: "text-black p-2 hover:bg-red-200 focus:ring-1 focus:ring-red-400",
+};
+
+const InputSize = {
+  sm: "text-sm px-2 py-1 border border-yellow-300 rounded-xs",
+  md: "text-md px-3 py-2 border border border-blue-300 rounded-md",
+  lg: "text-lg px-4 py-3 border border-green-300 rounded-lg",
+  xl: "text-xl px-5 py-4  border border-red-300 rounded-2xl",
+};
+
+const Icon = {
+  default: null,
+  search: <Search />,
+  mail: <Mail />,
+  location: <MapPin />,
+  number: <Hash />,
+};
 
 function FormInput({
   name = "input-field",
@@ -19,22 +49,34 @@ function FormInput({
   value,
   onChange,
   isRequired = false,
-  children
+  variant = "default",
+  size = "md",
+  className,
+  icon = "default",
 }: FormInputProps) {
+  const variantClass = colorCombination[variant] || colorCombination.default;
+  const sizeClass = InputSize[size] || InputSize.md;
+  const iconClass = Icon[icon] || null;
+
   return (
-    <div className="space-y-1">
-      <label className="text-sm font-normal">
+    <div className="flex flex-col">
+      <label>
         {label} {isRequired && <span className="text-red-600">*</span>}
       </label>
-      <input
-        id={name}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className="h-10 w-96 px-3 py-2 text-sm border border-gray-300 rounded-md"
-      />
+      <div className="relative">
+        {iconClass && (
+          <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black">{iconClass}</span>
+        )}
+        <input
+          id={name}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={`${iconClass ? "pl-10" : ""} ${variantClass} ${sizeClass} ${className} || ""`}
+        />
+      </div>
     </div>
   );
 }
