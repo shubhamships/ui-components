8b13478d3437cfce9781ea050eba08d6cf579013
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-21 11:53:18 +0530

    button component added

---
+++ aayushi/src/components/Button.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+export const Button = ({ loading, type, onClick }) => {
+    return (
+      <button
+        className="text-sm font-medium h-11 rounded-md w-full focus-visible:outline-none focus-visible:ring-2  max-w-sm mt-10 text-white bg-blue-800"
+        onClick={onClick}
+      >
+        {type}
+        {loading && (
+          <span>
+            <i className="fa-solid fa-spinner animate-spin ml-2"/>
+          </span>
+        )}
+      </button>
+    );
+  };
