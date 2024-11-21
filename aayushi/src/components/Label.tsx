c99e039be2d3089a7be6cf36dc9947edeb3151ca
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-21 12:00:31 +0530

    label component created

---
+++ aayushi/src/components/Label.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+
+export const Label = ({ type, className }) => {
+    return (
+      <div className="mb-2">
+        <label className={`text-sm mb-2 ${className}`}>{type} </label>
+      </div>
+    );
+  };
