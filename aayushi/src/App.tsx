74adf552089b79b92de3ba14b307f68002833908
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-22 12:30:08 +0530

    setup tailwindcss

--- aayushi/src/App.tsx
+++ aayushi/src/App.tsx
@@ -24,7 +24,8 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
