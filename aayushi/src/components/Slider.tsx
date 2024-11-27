0f43354f4aa949364135bbc9d2beae0fbd7bc98e
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-27 10:14:33 +0530

    button fixed

--- aayushi/src/components/Slider.tsx
+++ aayushi/src/components/Slider.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./Navbar";
 import imgSrc from "../assets/img.png";
 import Button from "./Button";
+import { Mail, CircleArrowRight } from "lucide-react";
 
 export const Slider = () => {
   return (
@@ -17,8 +18,25 @@ export const Slider = () => {
               <p className="text-lg font-normal mb-4 mt-2 leading-tight">
                 Submit your book to Reedsy Discovery for $50.
               </p>
-
-              <Button />
+              <div className="grid grid-cols-2 md:grid md:grid-cols-3 ">
+                <Button
+                  text="Sign in with Google"
+                  className="bg-white text-black py-1 px-2 border-white sm:pl-4 md:pl-8 mr-1 object-cover lg:pl-4 lg:py-1"
+                  icon={
+                    <img src="src/assets/img1.png" className="w-4 h-4 ml-0.5 mr-2 sm:mr-4 md:mr-6" alt="Google Icon" />
+                  }
+                />
+                <Button
+                  text="Sign up"
+                  className="bg-amber-400 border-amber-400 px-1 py-1/2 mx-2 sm:mx-3 md:mx-2 lg:px-2 lg:py-1 lg:mx-2 "
+                  icon={<Mail className="ml-2" />}
+                />
+                <Button
+                  text="Learn more"
+                  className="hidden max-w-max md:inline-flex md:border-2 border-white  px-2 py-1 mx-2"
+                  icon={<CircleArrowRight className="ml-2" />}
+                />
+              </div>
             </div>
 
             <img src={imgSrc} className="h-48 pl-20 pr-20 lg:block hidden lg:pr-5 lg:pl-5" />
