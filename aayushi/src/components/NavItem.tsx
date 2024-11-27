607b8902a210493eee093f535b5c8d2d3647e4c2
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-27 12:42:53 +0530

    optimised code

--- aayushi/src/components/NavItem.tsx
+++ aayushi/src/components/NavItem.tsx
@@ -1,16 +1,3 @@
-// export const NavItem = ({ href="", children="", className = "" }) => {
-//   return (
-//     <li>
-//       <a
-//         href={href}
-//         className={`hover:border-b-2 hover:border-yellow-400 hover:transition-all hover:ease-in-out ${className}`}
-//       >
-//         {children}
-//       </a>
-//     </li>
-//   );
-// };
-
 import { Link } from "react-router-dom";
 
 interface NavItemProps {
