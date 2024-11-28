46f9a8094d5cb695dfc3acf5b4257f3a0edbb030
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-11-28 14:36:20 +0530

    carditems added

--- aayushi/src/components/Intro.tsx
+++ aayushi/src/components/Intro.tsx
@@ -1,17 +1,36 @@
-import imgHand from '../assets/hand.gif'
+import imgHand from "../assets/hand.gif";
+import { Link } from "react-router-dom";
+import { ArrowDown } from "lucide-react";
 
 export const Intro = () => {
   return (
-    <div className="flex flex-row space-x-3 justify-center p-12 ">
-        <img src={imgHand} alt="image" className='h-36 opacity-80'/>
-        <div>
-            <strong className='text-purple-950'>Hello!</strong>
-            <div>I am Enkhmaa.</div>
-            <strong className='text-purple-950'>Interaction Designer</strong>
-            <div>Inspired By Functional Designs That</div>
-            <strong className='text-purple-950'>Highlight Human Experience.</strong>
+    <div className="">
+      <div className="flex flex-row space-x-8 ml-64 p-12 items-center mt-6 text-xl">
+        <img src={imgHand} alt="image" className="h-36 opacity-80 animate-slide-from-top" />
+        <div className="opacity-80">
+          <strong className="text-purple-950 animate-slide-from-top font-black">HELLO!</strong>
+          <div className="animate-slide-from-left">I AM ENKHMAA.</div>
+          <strong className="text-purple-950 animate-slide-from-right font-black">INTERACTION DESIGNER</strong>
+          <div className="animate-slide-from-left">INSPIRED BY FUNCTIONAL DESIGNS THAT</div>
+          <strong className="text-pink-900 animate-slide-from-right font-black">HIGHLIGHT HUMAN EXPERIENCE.</strong>
         </div>
-
+      </div>
+      <div className="underline decoration-1 flex flex-row space-x-60  font-semibold justify-center mt-16">
+        <Link
+          to="#"
+          className="flex items-center space-x-0 text-red-600 text-xl opacity-45 hover:text-red-600 hover:opacity-100"
+        >
+          <span>View Case Studies</span>
+          <ArrowDown />
+        </Link>
+        <Link
+          to="#"
+          className="flex items-center space-x-0 text-red-600 text-xl opacity-45 hover:text-red-600 hover:opacity-100"
+        >
+          <span>View Gallery</span>
+          <ArrowDown />
+        </Link>{" "}
+      </div>
     </div>
-  )
-}
+  );
+};
--- aayushi/src/pages/HomePage.tsx
+++ aayushi/src/pages/HomePage.tsx
@@ -1,12 +1,20 @@
 import { Intro } from "@/components/Intro"
 import { Navbar } from "../components/Navbar"
+import { CardItems } from "./CardItems"
 
 
 export const HomePage = () => {
   return (
     <div>
+     
         <Navbar/>
         <Intro/>
+        <div>
+        <h1 className="text-3xl font-extrabold text-zinc-600 pt-10 opacity-95">CASE STUDIES</h1>
+        <div className="h-0.5 mt-3 min-w-max mb-6 bg-purple-900"/>
+        </div>
+        <CardItems/>
+        
     </div>
   )
 }
