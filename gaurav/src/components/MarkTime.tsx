f580a5cf198cd85390b4276d86bf5d3a06bdf0cc
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2024-11-29 13:51:02 +0530

    Stored punch data to the local storage

--- gaurav/src/components/MarkTime.tsx
+++ gaurav/src/components/MarkTime.tsx
@@ -1,57 +1,52 @@
-// import { useDispatch, useSelector } from "react-redux";
 import Button from "./ui/Button";
 import Card from "./ui/Card";
-// import { punchIn, punchOut } from "@/redux/actions";
 import { useState } from "react";
 import Errors from "./ui/Errors";
 
 export const MarkTime = () => {
   const [error, setError] = useState(false);
   const [isCompleted, setIsCompleted] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(false);
-  const [punchData, setPunchData] = useState<{ id: number; time: string; type: string }[]>([]);
-  // const [fieldDisAbled, setFieldDisAbled] = useState("PUNCHIN");
-
-  // Redux
-  // const dispatch = useDispatch();
-  // const data = useSelector((state: any) => state.data.punchInDate);
-  // const punchData = useSelector((state: any) => state.data);
-  // const type = punchData.punchData[punchData.punchData.length - 1]?.type;
-
-  // console.log(punchData.punchData.id, "fsdfsd");
-
-  // on punch in - save time in punchintim and type in
-  // on punch out - save time in punchouttime and type out
+  const [punchInDisabled, setPunchInDisabled] = useState(false);
+  const [punchOutDisabled, setPunchOutDisabled] = useState(true);
+  const [punchData, setPunchData] = useState<{ id: number; time: string; type: string }[]>(() => {
+    const storedData = localStorage.getItem("punchData");
+    return storedData ? JSON.parse(storedData) : [];
+  });
+  const [totalHours, setTotalHours] = useState<number>(0);
 
   const handlePunchIn = () => {
     try {
-      // save data in iso string or epoch time
-      // if (punchData.punchData.length === 0 ||
-      //   type  === "OUT") {
-      //   dispatch(punchIn(new Date().toISOString()));
-      // }
       if (punchData.length === 0 || punchData[punchData.length - 1].type === "OUT") {
-        setPunchData([
+        const newPunchInData = [
           ...punchData,
           {
             id: punchData.length,
             time: new Date().toISOString(),
             type: "IN",
           },
-        ]);
-        setIsDisabled(true);
+        ];
+        setPunchData(newPunchInData);
+        localStorage.setItem("punchData", JSON.stringify(newPunchInData));
+        setPunchInDisabled(true);
+        setPunchOutDisabled(false);
       }
     } catch (error) {
       console.log("error", error);
     }
   };
+
+  // useEffect(() => {
+  //   // save disabled props in local storage
+  // },[location.pathname])
+
   console.log(punchData);
   const handlePunchOut = () => {
     const time = new Date();
     const punchOutTime = time.getTime();
     const punchInTime = new Date(punchData[punchData.length - 1].time).getTime();
-    // const punchInTime = new Date(punchData.length - 1).getTime(); // - epoch time in ms
     const totalTime = (punchOutTime - punchInTime) / 100;
+    setTotalHours(totalTime);
+
     console.log(totalTime);
     console.log(
       new Date(punchData[punchData.length - 1].time).getTime(),
@@ -65,28 +60,28 @@ export const MarkTime = () => {
     } else {
       console.log("success");
       if (punchData.length === 0 || punchData[punchData.length - 1].type === "IN") {
-        setPunchData([
+        const newPunchOutData = [
           ...punchData,
           {
             id: punchData.length,
             time: new Date().toISOString(),
             type: "OUT",
           },
-        ]);
+        ];
+        setPunchData(newPunchOutData);
+        localStorage.setItem("punchData", JSON.stringify(newPunchOutData));
       }
-      // if(punchData.punchData.length > 0 && type === "IN"){
-      //   dispatch(punchOut(new Date().toISOString()));
-      // }
+
       setError(false);
       setIsCompleted(true);
-      setIsDisabled(false);
+      setPunchInDisabled(false);
+      setPunchOutDisabled(true);
     }
   };
-  // console.log(punchData.punchData.id, "Data type")
-
-  // console.log("id", punchData.punchData.id);
-  // console.log(punchData.punchData, "punchData");
-  console.log("isCompleted : ", isCompleted + " |" + "isDisabled : " + isDisabled + " |" + " error : " + error);
+  console.log(
+    "isCompleted : ",
+    isCompleted + " |" + "punchInDisabled : " + punchInDisabled + " |" + " error : " + error,
+  );
   return (
     <>
       <div className="flex flex-col items-center h-screen w-full p-4 pt-11 bg-slate-50">
@@ -94,8 +89,10 @@ export const MarkTime = () => {
         <Card className="relative flex flex-col items-center h-full min-w-80 max-w-96 py-8 px-8 gap-2 bg-gray-50 shadow-sm overflow-auto">
           <Button
             title="Punch In"
-            disabled={isDisabled}
-            className={` ${isDisabled ? "cursor-not-allowed" : "cursor-pointer"} w-full sticky top-0`}
+            disabled={punchInDisabled}
+            className={` ${
+              punchInDisabled ? "cursor-not-allowed bg-opacity-75 hover:bg-opacity-75" : "cursor-pointer"
+            } w-full sticky top-0`}
             type=""
             // disabled={fieldDisAbled==='PUNCHIN'}
             onClick={handlePunchIn}
@@ -104,17 +101,23 @@ export const MarkTime = () => {
             <Button
               title="Punch Out"
               variant="success"
-              disabled={!isDisabled}
-              className={`${!isDisabled ? "cursor-not-allowed" : "cursor-pointer"} w-full`}
+              disabled={punchOutDisabled}
+              className={`${
+                punchOutDisabled ? "cursor-not-allowed bg-opacity-75 hover:bg-opacity-75" : "cursor-pointer"
+              } w-full`}
               onClick={handlePunchOut}
             />
-            <Errors name={error} errorDescription={`Time elapsed `} className="text-sm font-medium" />
+            <Errors
+              name={error}
+              errorDescription={`Time elapsed ${totalHours.toFixed(2)}`}
+              className="text-sm font-medium"
+            />
           </div>
           <div className="scroll-auto overflow-x-clip space-y-2 w-full mt-4">
             {punchData.map((item: any, index: number) => (
               <Card
-                className={`flex justify-center items-center h-14 w-ful gap-2 text-sm font-semibold  whitespace-nowrap border-none
-                ${item.type === "OUT" ? "bg-green-100" : "bg-blue-100"}
+                className={`flex justify-center items-center h-14 w-ful gap-2 text-sm font-semibold whitespace-nowrap border-none
+                ${item.type === "OUT" ? "bg-blue-50" : "bg-green-50"}
                 `}
                 variant="default"
                 key={index}
@@ -123,11 +126,11 @@ export const MarkTime = () => {
                   <div className={`text-sm tracking-wide `}>
                     {item.type === "OUT" ? (
                       <div className="text-sm text-success">
-                        Punch Out TIme {new Date(item.time).toLocaleTimeString()} {index}
+                        Punch Out TIme {new Date(item.time).toLocaleTimeString()}
                       </div>
                     ) : (
                       <div className="text-sm text-primary">
-                        Punch In Time {new Date(item.time).toLocaleTimeString()} {index}
+                        Punch In Time {new Date(item.time).toLocaleTimeString()}
                       </div>
                     )}
                   </div>
