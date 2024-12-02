7d5330914252631413ba7e1924a2d3170357bef5
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2024-12-02 15:19:42 +0530

    Added total time calculation feature

--- gaurav/src/components/MarkTime.tsx
+++ gaurav/src/components/MarkTime.tsx
@@ -1,10 +1,9 @@
 import Button from "./ui/Button";
 import Card from "./ui/Card";
 import { useState, useEffect } from "react";
-import Errors from "./ui/Errors";
 
 export const MarkTime = () => {
-  const [error, setError] = useState(false);
+  const [netTime, setNetTime] = useState<number>(0);
   const [isCompleted, setIsCompleted] = useState(false);
   const [punchInDisabled, setPunchInDisabled] = useState<boolean>();
   const [punchOutDisabled, setPunchOutDisabled] = useState<boolean>();
@@ -12,14 +11,12 @@ export const MarkTime = () => {
     const storedData = localStorage.getItem("punchData");
     return storedData ? JSON.parse(storedData) : [];
   });
-  const [totalHours, setTotalHours] = useState<number>(0);
   useEffect(() => {
     const storedButtonData = localStorage.getItem("buttonPunchInData");
     const storedButtonData1 = localStorage.getItem("buttonPunchOutData");
     setPunchInDisabled(storedButtonData ? JSON.parse(storedButtonData) : false);
     setPunchOutDisabled(storedButtonData1 ? JSON.parse(storedButtonData1) : true);
   }, []);
-
   const handlePunchIn = () => {
     try {
       if (punchData.length === 0 || punchData[punchData.length - 1].type === "OUT") {
@@ -32,7 +29,6 @@ export const MarkTime = () => {
           },
         ];
         setPunchData(newPunchInData);
-
         localStorage.setItem("punchData", JSON.stringify(newPunchInData));
       }
       setPunchInDisabled(true);
@@ -43,58 +39,44 @@ export const MarkTime = () => {
       console.log("error", error);
     }
   };
-
-  console.log(punchData);
   const handlePunchOut = () => {
     const time = new Date();
     const punchOutTime = time.getTime();
     const punchInTime = new Date(punchData[punchData.length - 1].time).getTime();
-    const totalTime = (punchOutTime - punchInTime) / 100;
-    setTotalHours(totalTime);
-
-    console.log(totalTime);
-    console.log(
-      new Date(punchData[punchData.length - 1].time).getTime(),
-      "total time in ms",
-      punchOutTime - punchInTime,
-    );
-    if (totalTime < 9) {
-      setError(true);
-      setIsCompleted(false);
-      console.log("You have not completed 9 hours");
-    } else {
-      console.log("success");
-      if (punchData.length === 0 || punchData[punchData.length - 1].type === "IN") {
-        const newPunchOutData = [
-          ...punchData,
-          {
-            id: punchData.length,
-            time: new Date().toISOString(),
-            type: "OUT",
-          },
-        ];
-        setPunchData(newPunchOutData);
-        localStorage.setItem("punchData", JSON.stringify(newPunchOutData));
-      }
-      setError(false);
-      setIsCompleted(true);
-      setPunchInDisabled(false);
-      setPunchOutDisabled(true);
-      localStorage.setItem("buttonPunchInData", JSON.stringify(false));
-      localStorage.setItem("buttonPunchOutData", JSON.stringify(true));
+    console.log("success");
+    if (punchData.length === 0 || punchData[punchData.length - 1].type === "IN") {
+      const newPunchOutData = [
+        ...punchData,
+        {
+          id: punchData.length,
+          time: new Date().toISOString(),
+          type: "OUT",
+        },
+      ];
+      setPunchData(newPunchOutData);
+      localStorage.setItem("punchData", JSON.stringify(newPunchOutData));
+      setNetTime((prev) => prev + (punchOutTime - punchInTime) / 1000); // time in ms
     }
+    setIsCompleted(true);
+    setPunchInDisabled(false);
+    setPunchOutDisabled(true);
+    localStorage.setItem("buttonPunchInData", JSON.stringify(false));
+    localStorage.setItem("buttonPunchOutData", JSON.stringify(true));
   };
-  console.log(
-    "isCompleted : ",
-    isCompleted + " |" + "punchInDisabled : " + punchInDisabled + " |" + " error : " + error,
-  );
-  console.log(punchInDisabled, punchOutDisabled);
-
   return (
     <>
       <div className="flex flex-col items-center h-screen w-full p-4 pt-11 bg-slate-50">
         <h1 className="text-3xl font-semibold text-primary m-4">Time Log</h1>
         <Card className="relative flex flex-col items-center h-full min-w-80 max-w-96 py-8 px-8 gap-2 bg-gray-50 shadow-sm overflow-auto">
+          <div className="text-sm font-semibold text-primary contrast-200 sticky">
+            {`Total time elapsed: ${
+              netTime < 60
+                ? netTime.toFixed(2) + " second"
+                : netTime < 3600
+                ? (netTime / 60).toFixed(2) + " minutes"
+                : (netTime / 3600).toFixed(2) + " hours"
+            } `}
+          </div>
           <Button
             title="Punch In"
             disabled={punchInDisabled}
@@ -113,36 +95,33 @@ export const MarkTime = () => {
               } w-full`}
               onClick={handlePunchOut}
             />
-            <Errors
-              name={error}
-              errorDescription={`Time elapsed ${totalHours.toFixed(2)}`}
-              className="text-sm font-medium"
-            />
           </div>
-          <div className="scroll-auto overflow-x-clip space-y-2 w-full mt-4">
-            {punchData.map((item: any, index: number) => (
-              <Card
-                className={`flex justify-center items-center h-14 w-full gap-2 text-sm font-semibold whitespace-nowrap 
-                ${item.type === "OUT" ? "border border-green-500" : "border border-blue-500"}
+          <div className="py-2 h-full">
+            <div className="scroll-auto overflow-x-clip space-y-2 w-full mt-4">
+              {punchData.map((item: any, index: number) => (
+                <Card
+                  className={`flex justify-center items-center px-14 h-14 w-full gap-2 text-sm font-semibold whitespace-nowrap
+                ${item.type === "OUT" ? "border border-green-500" : "border border-blue-400"}
                 `}
-                variant="default"
-                key={index}
-              >
-                <div className="flex justify-around items-center gap-4">
-                  <div className={`text-sm tracking-wide `}>
-                    {item.type === "OUT" ? (
-                      <div className="text-sm text-success">
-                        Punch Out TIme {new Date(item.time).toLocaleTimeString()}
-                      </div>
-                    ) : (
-                      <div className="text-sm text-primary">
-                        Punch In Time {new Date(item.time).toLocaleTimeString()}
-                      </div>
-                    )}
+                  variant="default"
+                  key={index}
+                >
+                  <div className="flex justify-around items-center gap-4">
+                    <div className={`text-sm tracking-wide `}>
+                      {item.type === "OUT" ? (
+                        <div className="text-sm text-success">
+                          Punch Out TIme {new Date(item.time).toLocaleTimeString()}
+                        </div>
+                      ) : (
+                        <div className="text-sm text-primary">
+                          Punch In Time {new Date(item.time).toLocaleTimeString()}
+                        </div>
+                      )}
+                    </div>
                   </div>
-                </div>
-              </Card>
-            ))}
+                </Card>
+              ))}
+            </div>
           </div>
         </Card>
       </div>
