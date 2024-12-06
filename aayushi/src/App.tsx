a7161efc00ca8eb46bc4dac729ed77bd88d10b41
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-12-06 11:11:57 +0530

    files changed

--- aayushi/src/App.tsx
+++ aayushi/src/App.tsx
@@ -1,22 +1,5 @@
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./Root";
-import { Carousel } from "./components/UserCarousel";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root/>,
-  },
-  {
-    path:"/carousel",
-    element: <Carousel/>,
-  },
-
-  
-]);
-
-export default function App() {
-  return <RouterProvider router={router} />;
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello Aayushi!</h1>;
 }
+
+export default App;
