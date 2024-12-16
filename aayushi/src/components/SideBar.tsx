72da11cf7062c0ee348618eec82437e97c2f45ec
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-12-16 18:54:49 +0530

    added transition to the sidebar

--- aayushi/src/components/SideBar.tsx
+++ aayushi/src/components/SideBar.tsx
@@ -1,7 +1,18 @@
-import { LayoutDashboard, Box, Users, Package2, Truck, Calculator, Wallet, Layers } from "lucide-react";
+import {
+  LayoutDashboard,
+  Box,
+  Users,
+  Package2,
+  Truck,
+  Calculator,
+  Wallet,
+  Layers,
+  CircleChevronRight,
+  CircleChevronLeft,
+} from "lucide-react";
 import { NavItem } from "./NavItem";
 import { NavIcon } from "./NavIcon";
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const sidebarItems = [
   { id: 1, icon: <LayoutDashboard />, text: "Dashboard" },
@@ -15,78 +26,54 @@ const sidebarItems = [
 ];
 
 export const Sidebar = ({ className, colorScheme }: { className: string; colorScheme?: any }) => {
-  const [hoverId, setHoverId] = useState<number | null>(null);
-  const [activeId, setActiveId] = useState<number | null>(null); // Initialize activeId as null
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  const handleMouseEnter = (index: number) => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    setHoverId(index);
-  };
-
-  const handleMouseLeave = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    timeoutRef.current = setTimeout(() => {
-      setHoverId(null);
-    }, 500);
-  };
+  const [activeId, setActiveId] = useState<number | null>(null);
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
   const handleClick = (index: number) => {
-    setActiveId(index); 
+    setActiveId(index);
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+
+  const iconClass =
+    "text-blue-50 bg-blue-600 rounded-full z-10 cursor-pointer transform transition-transform ease-in-out duration-200";
+
   return (
-    <div className={`h-screen flex flex-row space-8 gap-2`}>
-      <div className={`flex flex-col items-center space-y-4 px-1 py-4 rounded-md cursor-pointer ${className}`}>
-        {sidebarItems.map((item, index) => (
+    <div className={`flex flex-row space-8 gap-2 relative font-semibold`}>
+      <div className="absolute top-0 -right-7 p-4 z-10">
+        {!isSidebarVisible && <CircleChevronRight className={iconClass} onClick={toggleSidebar} />}
+        {isSidebarVisible && <CircleChevronLeft className={iconClass} onClick={toggleSidebar} />}
+      </div>
+
+      <div
+        className={`flex flex-col space-y-4 px-4 py-4 w-full transform transition-transform ease-in-out duration-200 ${className} 
+    ${isSidebarVisible ? "translate-x-0" : "-translate-x-1"}`}
+      >
+        {/* clsx */}
+        {sidebarItems.map((item, _) => (
           <div
-            key={item.id} 
-            className={`group flex flex-col items-center p-2 rounded-md 
-              ${activeId === item.id ? `${colorScheme.clickBg} ${colorScheme.clickBorder} ${colorScheme.text}` : ''}
-              ${colorScheme.hoverBorder} ${colorScheme.hoverBg}`}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick(item.id)} 
+            key={item.id}
+            className={`group flex flex-row items-center space-x-2 p-2 rounded-md cursor-pointer hover:font-semibold 
+        ${activeId === item.id ? `${colorScheme.clickBg} ${colorScheme.clickBorder} ${colorScheme.text}` : ""} 
+        ${colorScheme.hoverBorder} ${colorScheme.hoverBg}`}
+            onClick={() => handleClick(item.id)}
           >
             <NavIcon
               icon={item.icon}
-              className={`group-hover:${colorScheme.text} transition-all`}
+              className={`group-hover:${colorScheme.text} transform transition-transform duration-500 ease-out group-hover:translate-x-1`}
             />
+
+            {isSidebarVisible && (
+              <NavItem
+                text={item.text}
+                className={`group-hover:${colorScheme.text} transform duration-500 ease-in-out transition-transform group-hover:translate-x-1`}
+              />
+            )}
           </div>
         ))}
       </div>
-
-      {hoverId !== null && (
-        <div
-          className={`flex flex-col space-y-4 px-4 rounded-md py-4 w-full ${className}`}
-          onMouseEnter={() => setHoverId(hoverId)}
-          onMouseLeave={handleMouseLeave}
-        >
-          {sidebarItems.map((item, index) => (
-            <div
-              key={item.id} 
-              className={`group flex flex-row items-center space-x-2 p-2 rounded-md cursor-pointer hover:font-semibold
-                ${activeId === item.id ? `${colorScheme.clickBg} ${colorScheme.clickBorder} ${colorScheme.text}` : ''}
-                ${colorScheme.hoverBorder} ${colorScheme.hoverBg}`}
-              onMouseEnter={() => handleMouseEnter(index)}
-              onClick={() => handleClick(item.id)}
-            >
-              <NavIcon
-                icon={item.icon}
-                className={`group-hover:${colorScheme.text} transition-all`}
-              />
-              <NavItem
-                text={item.text}
-                className={`group-hover:${colorScheme.text} transition-all`}
-              />
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 };
--- aayushi/src/pages/Dashboard.tsx
+++ aayushi/src/pages/Dashboard.tsx
@@ -5,11 +5,11 @@ import { Button } from "../components/Button";
 export const Dashboard = () => {
   return (
     <div className="h-screen flex flex-col">
-    <div className="flex justify-between items-center mx-6 pb-4 border-b-2 border-gray-200">
+    <div className="flex justify-between items-center mx-6 pb-2 border-b-2 border-gray-200">
       <a href="/">
-        <img src={Logo} className="h-6 mt-8" />
+        <img src={Logo} className="h-6 mt-2" />
       </a>
-      <Button type={"Logout"} className="w-20" />
+      <Button type={"Logout"} className="w-auto px-2 py-0.5 mt-2" />
     </div>
 
     <div className="flex flex-1">
@@ -19,8 +19,8 @@ export const Dashboard = () => {
         hoverText: 'group-hover:text-blue-500',
         hoverBorder: 'hover:border-blue-50',
         clickBorder:'border-blue-50',
-        clickBg:'bg-blue-200',
-        hoverBg: 'hover:bg-blue-200',
+        clickBg:'bg-white',
+        hoverBg: 'hover:bg-white',
       }}/>
 
     </div>
