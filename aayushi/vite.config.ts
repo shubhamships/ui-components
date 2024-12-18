e157fe7695f02d2dfeb6061413b201ff82bffd10
aayushijain-05 <aayushi.jain@shipglobal.in>
2024-12-18 14:51:13 +0530

    configuration added

--- aayushi/vite.config.ts
+++ aayushi/vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    federation({
+   name:'featureA',
+   filename:'remoteEntry.js',
+   remotes: {
+    remote_app: "http://localhost:3000/assets/remoteEntry.js",
+},
+   exposes:{
+    './App':'./src/App.tsx'
+   }   
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
